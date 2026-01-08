e7d59b666fb54c73523b7b42b4f240296e3ffb8f
Anton <70934030+friendlyantz@users.noreply.github.com>
2026-01-08 12:41:13 +1100

    WEBVIEW UX: add webview detection early on. dedicated controller for unsupported browser seems to add too much complexity with no success on telegram WebView

--- app/controllers/challenge_stories_controller.rb
+++ app/controllers/challenge_stories_controller.rb
@@ -21,10 +21,6 @@ class ChallengeStoriesController < ApplicationController
   end
 
   def show
-    if likely_webview?
-      @likely_webview_shared_url = params[:url] || root_url
-    end
-
     @challenge_story = ChallengeStory
       .includes(
         challenge_participants: [:user, :challenge_check_ins],
