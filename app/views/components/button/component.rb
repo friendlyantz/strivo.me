57a92856515f3382386fce19c06fb2962b1f1583
Anton <70934030+friendlyantz@users.noreply.github.com>
2026-01-10 20:52:45 +1100

    UI: Make Story status madge less confusing. minor fix
    badge looked like a button according to user feedback

--- app/views/components/button/component.rb
+++ app/views/components/button/component.rb
@@ -65,6 +65,20 @@ class Button::Component
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
       SVG
+    when :light_bolt
+      # Stashed / unused icon
+      <<~SVG.html_safe
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+        </svg>
+      SVG
+    when :check_with_circle
+      # Stashed / unused icon
+      <<~SVG.html_safe
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      SVG
     end
   end
 
